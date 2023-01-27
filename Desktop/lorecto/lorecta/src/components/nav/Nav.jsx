3de4e93b2c956a4baa38e3f7b71834cde42da6eb
Adewumi <owonikokoadewunmi@gmail.com>
2023-01-27 08:41:43 +0100

    nav contents

--- Desktop/lorecto/lorecta/src/components/nav/Nav.jsx
+++ Desktop/lorecto/lorecta/src/components/nav/Nav.jsx
@@ -5,8 +5,17 @@ const nav = () => {
   return (
     <div>
         <div>
-            <div><img src={logo} alt="" /></div>
-            <div></div>
+            <div><img src={logo} alt="" />
+            
+            <ul>
+                    <li>Home</li>
+                    <li>Page</li>
+                    <li>Listings</li>
+                    <li>Profile</li>
+                </ul>
+            </div>
+
+            
             <div></div>
         </div>
     </div>
