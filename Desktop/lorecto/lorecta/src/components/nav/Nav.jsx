a29309756ed712a5ba7699fce355fe0bab5e2064
Adewumi <owonikokoadewunmi@gmail.com>
2023-01-27 08:27:50 +0100

    logo

---
+++ Desktop/lorecto/lorecta/src/components/nav/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from "../../Images/Logo.jpg"
+
+const nav = () => {
+  return (
+    <div>
+        <div>
+            <div><img src={logo} alt="" /></div>
+            <div></div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default nav
