10ca404678653adcafa6db442e1ea6703ff1c55e
Adewumi <owonikokoadewunmi@gmail.com>
2023-02-04 23:34:59 +0100

    added icon to the nav content

--- Desktop/lorecto/lorecta/src/components/nav/Nav.jsx
+++ Desktop/lorecto/lorecta/src/components/nav/Nav.jsx
@@ -1,25 +1,43 @@
-import React from 'react'
-import logo from "../../Images/Logo.jpg"
+import React from "react";
+import logo from "../../Images/Logo.jpg";
+import dropdown from "../../Images/En.jpg";
+import vector from "../../Images/Vector 14.jpg";
+import signIn from "../../Images/SignIn.png";
+import "./nav.css";
 
 const nav = () => {
   return (
     <div>
+      <div className="navbar">
         <div>
-            <div><img src={logo} alt="" />
-            
-            <ul>
-                    <li>Home</li>
-                    <li>Page</li>
-                    <li>Listings</li>
-                    <li>Profile</li>
-                </ul>
-            </div>
-
-            
-            <div></div>
+          <img src={logo} alt="" />
         </div>
-    </div>
-  )
-}
+        <div className="navlist">
+          <ul>
+            <li>Home</li>
+            <li>Page</li>
+            <li>Listings</li>
+            <li>Profile</li>
+          </ul>
+        </div>
 
-export default nav
+        <div className="contact-signIn">
+          <div className="nav-drop">
+            <img src={dropdown} alt="" />
+            <img src={vector} alt="" />
+            <img src={signIn} alt="" />
+          </div>
+
+          <div>
+            <span>Add Listing</span>
+            <span class="material-symbols-outlined">
+post_add
+</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default nav;
